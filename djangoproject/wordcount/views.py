75a0eb60b528205f70719bbe526378dd13fb8dd2
leedongjae0611 <ted4562000@naver.com>
2019-12-11 16:34:15 +0900

    dictionary itemp

--- djangoproject/wordcount/views.py
+++ djangoproject/wordcount/views.py
@@ -11,5 +11,13 @@ def about(request):
 def result(request):
     text = request.GET['fulltext']
     words = text.split(' ')
-
-    return render(request, 'result.html', {'full':text, 'total' : len(words)})
+    word_dictionary = {}
+    for i in words: #{
+        if i in word_dictionary: #{ 
+            word_dictionary[i] += 1    
+        #}
+        else: #{
+            word_dictionary[i] = 1
+        #}
+    #}
+    return render(request, 'result.html', {'full':text, 'total' : len(words), 'dictionary':word_dictionary.items()})
