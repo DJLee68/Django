31b9d2d372c7206a9d638d0fe00c1d00ac36387b
leedongjae0611 <ted4562000@naver.com>
2020-02-12 00:43:30 -0800

    media setting

--- blogtest/blogtest/settings.py
+++ blogtest/blogtest/settings.py
@@ -124,8 +124,14 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio', 'static')
 ]
+
+
 # static 파일들이 현재 어디에 있는지를 쓰는 곳
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # static 파일들이 어디로 모일 것인 지 쓰는 곳
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
