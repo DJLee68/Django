a02cee054390f1d916466fd23bed7944c86cee62
leedongjae0611 <ted4562000@naver.com>
2020-02-15 14:49:33 -0800

    setting

--- blogtest/blogtest/settings.py
+++ blogtest/blogtest/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blogtest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['blogtest/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
