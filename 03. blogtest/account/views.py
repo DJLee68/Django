c0deafb4fe6beba7edc7143f39b13b025a8a821b
leedongjae0611 <ted4562000@naver.com>
2020-02-19 00:53:39 -0800

    login view

--- 03. blogtest/account/views.py
+++ 03. blogtest/account/views.py
@@ -16,5 +16,15 @@ def signup(request): #{
 #}
 
 def login(request): #{
-    return render(request, 'login.html')
+    if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+                auth.login(request, user)
+                return redirect('home')
+        else:
+                return render(request, 'login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
 #}
