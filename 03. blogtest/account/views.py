30e52824a008472221e033704f4e360bccad8d98
leedongjae0611 <ted4562000@naver.com>
2020-02-19 00:39:26 -0800

    importing

--- 03. blogtest/account/views.py
+++ 03. blogtest/account/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib import auth
 # Create your views here.
 
 
 def signup(request): #{
+
     return render(request, 'signup.html')
 #}
 
