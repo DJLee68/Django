b16f66db4ea6b67d8fa6f634f644dd1af0d0441b
leedongjae0611 <ted4562000@naver.com>
2020-02-19 00:42:08 -0800

    signup views

--- 03. blogtest/account/views.py
+++ 03. blogtest/account/views.py
@@ -5,7 +5,13 @@ from django.contrib import auth
 
 
 def signup(request): #{
-
+    if request.method == 'POST': #{
+        if request.POST['password1'] == request.POST['password2']: #{
+            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+            auth.login(request, user)
+            return redirect('blog')
+        #}
+    #}
     return render(request, 'signup.html')
 #}
 
