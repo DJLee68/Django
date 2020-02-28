a980078ccd0cb291e8b0d17616828477f329db49
leedongjae0611 <ted4562000@naver.com>
2020-02-28 00:50:08 -0800

    home

--- 03. blogtest/makeblog/views.py
+++ 03. blogtest/makeblog/views.py
@@ -15,7 +15,7 @@ def home(request): #{
     page = request.GET.get('page')
     #request된 페이지를 얻어온 뒤 return 해 준다.
     posts = paginator.get_page(page)
-    return render(request, 'home.html', {'blogs' : blogs})
+    return render(request, 'home.html', {'blogs' : blogs, 'posts' : posts})
 #}
 
 def detail(request, blog_id): #{
