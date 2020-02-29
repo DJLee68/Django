9f5a8a12ed2e933adc35f8c53cc3e8d8810d0a07
leedongjae0611 <ted4562000@naver.com>
2020-02-29 01:34:32 -0800

    random seed

--- fake.py
+++ fake.py
@@ -4,6 +4,11 @@ myfake = Faker()
 
 # Faker의 메소드를 통해 어떤 종류의 가짜데이터를 뽑아낼지 결정 가능
 
+Faker.seed(1)
+
 print(myfake.name()) #가짜 이름
 print(myfake.address()) #가짜 주소
-print(myfake.text())
+#print(myfake.text())
+#print(myfake.state())
+#print(myfake.sentence())
+print(myfake.random_number(1))
