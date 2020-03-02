d0ac5ad727533a38f83cf186f5dd392a7d4275b9
leedongjae0611 <ted4562000@naver.com>
2020-03-02 00:39:48 -0800

    form

--- 03. blogtest/makeblog/form.py
+++ 03. blogtest/makeblog/form.py
@@ -1,2 +1,9 @@
 from django import forms
 from .models import Makeblog
+
+class BlogPost(forms.ModelForm): #{
+    class Meta: #{
+        model = Makeblog
+        fields = ['title', 'body']  
+    #}
+#}
