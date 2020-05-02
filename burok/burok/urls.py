c6cc62a31934167758de27392b3c0dc0c4154a79
leedongjae0611 <ted4562000@naver.com>
2020-05-02 18:42:12 +0900

    split url

--- burok/burok/urls.py
+++ burok/burok/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import viewcrud.views
+import viewcrud.urls
+import classcrud.views
+import classcrud.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', viewcrud.views.welcome, name='welcome'),
+    path('funccrud/', include(viewcrud.urls)),
+    path('classcrud/', include(classcrud.urls)),
 ]
