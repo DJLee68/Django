28b6f509b4dcc9b1a980a6487293cf9ef6d6cb40
leedongjae0611 <ted4562000@naver.com>
2020-05-04 01:27:01 +0900

    end

--- burok/viewcrud/views.py
+++ burok/viewcrud/views.py
@@ -14,7 +14,28 @@ def read(request): #{
     return render(request, 'viewcrud/funccrud.html', {'blogs':blogs})
 #}
 
-def create(request)
+def create(request): #{
+    # 새로운 데이터 새로운 블로그 글을 저장하는 역할 == POST
+    # 글쓰기 페이지를 띄워주는 역할 == GET
+
+    if request.method == 'POST': #{
+        # 입력된 블로그 글들을 저장해라
+        form = NewBlog(request.POST)
+        if form.is_valid:
+            post = form.save(commit=False)
+            post.pub_data = timezone.now()
+            post.save()
+            return redirect('home')
+    #}
+
+    else: #{
+        # 단순히 입력받을 수 있는 form을 띄워줘라
+        form = NewBlog()
+        return render(request, 'viewcrud/new.html', {'form' : form})
+    #}
+
+    return 
+#}
 
 
 
