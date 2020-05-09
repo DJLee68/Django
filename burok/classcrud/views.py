a335c67a1b381ec7c6dfae18d9e35a98ea4cfa73
leedongjae0611 <ted4562000@naver.com>
2020-05-09 17:41:57 +0900

    default template name

--- burok/classcrud/views.py
+++ burok/classcrud/views.py
@@ -8,25 +8,25 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import ClassBlog
 # Create your views here.
 
-class BlogView(ListView): #{ html 템플릿 : 블로그 리스트를 담은 html
+class BlogView(ListView): #{ html 템플릿 : 블로그 리스트를 담은 html --> (소문자모델)_list.html
     model = ClassBlog
 #}
-class BlogCreate(CreateView): #{ html: form(입력공간)을 가지고 있는 html
-    model = ClassBlog
-    fields = ['title', 'body']
-    success_url = reverse_lazy('list')
-#}
-
-class BlogDetail(DetailView): #{ html: 상세 페이지를 담은 html
-    model = ClassBlog
-#}
-class BlogUpdate(UpdateView): #{ html: form(입력공간)을 가지고 있는 html
+class BlogCreate(CreateView): #{ html: form(입력공간)을 가지고 있는 html --> (소문자모델)_form.html
     model = ClassBlog
     fields = ['title', 'body']
     success_url = reverse_lazy('list')
 #}
 
-class BlogDelete(DeleteView): #{ html: '이거 진짜 지울거야?' html
+class BlogDetail(DetailView): #{ html: 상세 페이지를 담은 html --> (소문자모델)_detail.html
+    model = ClassBlog
+#}
+class BlogUpdate(UpdateView): #{ html: form(입력공간)을 가지고 있는 html --> (소문자모델)_form.html
+    model = ClassBlog
+    fields = ['title', 'body']
+    success_url = reverse_lazy('list')
+#}
+
+class BlogDelete(DeleteView): #{ html: '이거 진짜 지울거야?' html --> (소문자모델)_confirm_delete.html
     model = ClassBlog
     success_url = reverse_lazy('list')
 #}
