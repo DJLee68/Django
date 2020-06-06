4ebe7eaa61107b01a032aa967c753ec1d46746c6
leedongjae0611 <ted4562000@naver.com>
2020-06-06 16:30:14 +0900

    import

--- firstrest/post/api_views.py
+++ firstrest/post/api_views.py
@@ -1,13 +1,25 @@
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-from django.http import Http404
-from rest_framework.views import APIView
+#데이터 처리 대상
+from Posts.models import Post
+from Posts.serializers import PostSerializer
+
+# status에 따라 직접 Response를 처리할 것
+from django.http import Http404 # Get Object or 404 직접 구현
 from rest_framework.response import Response
 from rest_framework import status
 
-class SnippetList(APIView): #{
+# APIView를 상속받은 CBV
+from rest_framework.views import APIView
+#Postdetail 클래스의 get_object 메소드 대신 이거 써도 된다
+# from django.shortcuts import get_object_or_404
+
+class PostList(APIView): #{
     def get(self, request, format=None): #{
-        snippets = Snippet.object.all()
-        serializer = SnippetSerializer(snippets, many=True)    
+        Posts = Post.object.all()
+        serializer = PostSerializer(Posts, many=True)
+        return Response(serializer.data)    
+    #}
+
+    def put(self, request, pk, format=None): #{
+        
     #}
 #}
