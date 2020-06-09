8cba3c08181a86258ded0791eae1cb4b0fd5f486
leedongjae0611 <ted4562000@naver.com>
2020-06-09 19:58:34 +0900

    mixin finish

--- firstrest/post/serializer.py
+++ firstrest/post/serializer.py
@@ -6,4 +6,5 @@ class PostSerializer(serializers.ModelSerializer): #{
         model = Post
         fields = '__all__' # or fields = ['title', 'body'] we can include 'id' also. 웬만하면 튜플로
         read_only_fields = ('title',)
+        # you can add 'write only' option as well.
 #}
