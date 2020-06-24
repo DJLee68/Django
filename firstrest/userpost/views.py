f1c809610d137503681a94ca9acf810283d2cc4a
leedongjae0611 <ted4562000@naver.com>
2020-06-24 21:33:29 +0900

    logged in user

--- firstrest/userpost/views.py
+++ firstrest/userpost/views.py
@@ -8,4 +8,16 @@ from rest_framework import viewsets
 class UserPostViewSet(viewsets.ModelViewSet): #{
     queryset = UserPost.objects.all()
     serializer_class = UserSerializer 
+
+    def get_queryset(self): #{
+        # 여기 내부에서 쿼리셋을 관리하고 queryset 리턴하는게 효율적!
+        qs = super().get_queryset()
+        # qs = qs.filter(author__id = 2)
+
+        # 지금 로그인한 유저의 글만 필터링 해라
+        qs = qs.filter(author=self.request.user)
+        
+        # .filter .exclude
+        return qs
+    #}
 #}
