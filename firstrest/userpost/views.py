2afefc56c2b6782c0a33f958e24e07e613898900
leedongjae0611 <ted4562000@naver.com>
2020-06-24 20:50:54 +0900

    migrate

--- firstrest/userpost/views.py
+++ firstrest/userpost/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from userpost.models import UserPost
+from userpost.serializer import UserSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+
+class UserPostViewSet(viewsets.ModelViewSet): #{
+    queryset = UserPost.objects.all()
+    serializer_class = UserSerializer 
+#}
