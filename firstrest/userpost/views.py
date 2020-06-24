7b9838365237c8c3fd14f1875b05aad3c028c3eb
leedongjae0611 <ted4562000@naver.com>
2020-06-24 21:39:54 +0900

    search

--- firstrest/userpost/views.py
+++ firstrest/userpost/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from userpost.models import UserPost
 from userpost.serializer import UserSerializer
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 # Create your views here.
 
@@ -9,6 +10,11 @@ class UserPostViewSet(viewsets.ModelViewSet): #{
     queryset = UserPost.objects.all()
     serializer_class = UserSerializer 
 
+    # 검색 기능
+    filter_backends = [SearchFilter]
+    search_fields = ('title',) # 어떤 column을 기반으로 검색할 것인지! 무조건 튜플을 인풋으로 받는다
+
+    # 필터 기능
     def get_queryset(self): #{
         # 여기 내부에서 쿼리셋을 관리하고 queryset 리턴하는게 효율적!
         qs = super().get_queryset()
@@ -21,7 +27,7 @@ class UserPostViewSet(viewsets.ModelViewSet): #{
         # 만약 로그인이 안되어있다면, 비어있는 쿼리셋을 리턴해라
         else:
             qs = qs.none()
-            
+
         # .filter .exclude
         return qs
     #}
