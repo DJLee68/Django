4b21c4a3aa7c51c1c91599d38ad8926a34acbc5b
leedongjae0611 <ted4562000@naver.com>
2020-06-24 20:54:53 +0900

    user create

--- firstrest/userpost/models.py
+++ firstrest/userpost/models.py
@@ -8,3 +8,7 @@ class UserPost(models.Model): #{
     body = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, default = 1, on_delete=models.CASCADE)
 #}
+# Default = 1 means 유저를 1번부터 시작한다!
+
+# 1번 유저 - dj / 1234
+# 2번 유저 - jm / 1234
