ebf0031799eabee2680704ba47acfacf78f03dc7
leedongjae0611 <ted4562000@naver.com>
2020-07-02 10:28:59 -0700

    urls

--- drf_project/mystorage/urls.py
+++ drf_project/mystorage/urls.py
@@ -1,2 +1,6 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from mystorage import views
 
+router = DefaultRouter()
+router.register('essay', views.PostViewSet)
