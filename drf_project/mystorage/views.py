55ccb7dca1664d3ca636245785406cdde97ee182
leedongjae0611 <ted4562000@naver.com>
2020-07-02 10:34:23 -0700

    temp view

--- drf_project/mystorage/views.py
+++ drf_project/mystorage/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from .models import Essay
+from .serializers import EssaySerializer
 
 # Create your views here.
 
 
-class PostViewSet(viewsets.ModelViewSet)
+class PostViewSet(viewsets.ModelViewSet): #{
+    queryset = Essay.objects.all()
+    serializer_class = 
+#}
