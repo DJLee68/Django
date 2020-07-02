9d43df920fcb3c767bc7ee408a43c57c3e3df0bc
leedongjae0611 <ted4562000@naver.com>
2020-07-02 11:38:21 -0700

    user

--- drf_project/mystorage/models.py
+++ drf_project/mystorage/models.py
@@ -8,4 +8,5 @@ class Essay(models.Model): #{
     author = models.ForeignKey(settings.AUTH_USER_MODEL, default = 1, on_delete = models.CASCADE)
     title = models.CharField(max_length=30)
     body = models.TextField()
+
 #}
